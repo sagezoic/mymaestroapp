e6f65c2269a46c1b45daf7ec3a16a81a9f47c0bc
Sunbeam <rakesh@dateze.in>
2023-08-18 15:03:41 +0530

    minor changes

--- src/main/java/com/app/controller/UserAuthController.java
+++ src/main/java/com/app/controller/UserAuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ import com.app.service.UserService;
 
 @RestController
 //@RequestMapping("/users")
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 public class UserAuthController {
 	
 	@Autowired
