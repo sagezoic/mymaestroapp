607fd6e1efe9f8ec326954e2811eef4071008128
sourabh <sourabhp0295@gmail.com>
2023-08-19 16:09:50 +0530

    updated in user controller

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -5,9 +5,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -73,7 +75,7 @@ public class UserController {
 		return ResponseEntity.ok(userService.downloadProfileImage(userId));
 	}
 	
-	@PostMapping(value="/delete/{userId}")
+	@DeleteMapping(value="/delete/{userId}")
 	public ResponseEntity<?> deleteUser(@PathVariable Long userId ){
 		
 		System.out.println("in user delete controller");
@@ -81,7 +83,7 @@ public class UserController {
 		return ResponseEntity.ok("user deleted Successfully");
 	}
 	
-	@PostMapping("/edit")
+	@PutMapping("/edit")
 	public ResponseEntity<?> editUser(@RequestBody UserDto user){
 		
 		System.out.println("in user edit controller");
